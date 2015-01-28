2086220d5c8c2ef456522b9c6d2590fc3452bbba
Gael Hopp <galougaga@gmail.com>
2015-01-28 14:50:35 +0100

    ajout fichiers

---
+++ app/components/cover/artisteCoverController.js
@@ -0,0 +1,46 @@
+app.controller('ArtisteCoverCtrl', ['$scope', '$resource', '$rootScope', 'Auth','routeRessource', '$location', '$cookies','$sce',
+ function ($scope, $resource, $rootScope, Auth, routeRessource, $location, $cookies,$sce){
+
+ 
+
+var controller = this;
+
+$rootScope.lienRandomItemByGenre = routeRessource.RandomItemByArtiste;
+
+
+$rootScope.randomItem;
+
+
+ $scope.launchRandomTrack = function(idArtiste){
+
+		var Res = $resource($scope.lienRandomItemByGenre,{},
+		{
+	        'query': {
+	            method: 'GET',
+	            isArray: true,
+	            headers: { 
+	              "Authorization" : 'WSSE profile="UsernameToken"',
+	              "X-wsse" : Auth.getUser().wsse
+	            },
+	            params:{id:"@id"}
+	        }
+        });
+		
+				Res.query(
+					{id:idArtiste},
+					function(mess){ 
+						$rootScope.randomItem = mess;
+
+						$rootScope.randomItem[0].sources = [{src: $sce.trustAsResourceUrl($rootScope.randomItem[0].url), type:"audio/mp3"}];
+						
+						$rootScope.launchPlay($rootScope.randomItem);
+						
+					},
+					function(error){ $rootScope.randomItem = error.data; });
+		
+	}
+
+	
+
+
+}]);

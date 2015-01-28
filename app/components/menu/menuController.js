c2cb25cb635cdc6f5e80934afeed406592b13990
joris favier <joris.favier@gmail.com>
2015-01-28 09:31:33 +0100

    fist step on the playlist call

--- app/components/menu/menuController.js
+++ app/components/menu/menuController.js
@@ -1,4 +1,5 @@
-app.controller('menuController',['$scope','$rootScope',"$sce",function ($scope, $rootScope,$sce) {
+app.controller('menuController',['$scope','$rootScope',"$sce",'routeRessource','Auth','$resource',
+  function ($scope, $rootScope,$sce, routeRessource, Auth, $resource) {
   
 
 // Playlists to get
@@ -31,6 +32,49 @@ app.controller('menuController',['$scope','$rootScope',"$sce",function ($scope, 
   $scope.deletePlaylist = function(id){
 
     //TO DO
+  }
+
+  $rootScope.launchPlaylist = function(idplaylist){
+
+    var PlaylistDetail = $resource(routeRessource.PlaylistDetail,{},
+    {
+      query: {
+        method: 'GET',
+        isArray: true,
+        headers: { 
+          "Authorization" : 'WSSE profile="UsernameToken"',
+          "X-wsse" : Auth.getUser().wsse
+        },
+        params:{iduser: "@iduser", id:"@id"}
+      }
+    });
+
+    var playlist = PlaylistDetail.query({iduser: Auth.getUser().id, id: idplaylist},
+      function(){
+        var track = playlist[0].iditem;
+        if(track.length==0)
+          return;
+        $rootScope.playlist = [];
+        if(Array.isArray(track)){
+          for(var i=0;i<track.length;i++){
+            $rootScope.playlist.push(track[i]);
+            track[i].sources = [{src: $sce.trustAsResourceUrl(track[i].url), type:"audio/mp3"}];
+          }
+        }
+        else if($.inArray(track, $rootScope.playlist)==-1){
+          $rootScope.playlist.push(track);
+          track.sources = [{src: $sce.trustAsResourceUrl(track.url), type:"audio/mp3"}];
+        }
+        $rootScope.playing = true;
+        $rootScope.small = false;
+        console.log($rootScope.playlist);
+        
+      },
+      function(error){
+        console.log("error");
+      }
+    );
+    
   }
 
 

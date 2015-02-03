3d8ea45f8b35d9a38c39e3d0efa84d5751ec737a
joris favier <joris.favier@gmail.com>
2015-02-03 21:29:09 +0100

    fix language

--- app/shared/clickLink/clickLinkDirective.js
+++ app/shared/clickLink/clickLinkDirective.js
@@ -1,7 +1,7 @@
 app.directive('clickLink', ['$location', function ($location) {
 
     return {
-    	template: '<a href=""><i class="icon-cancel-circle"></i></a>',
+    	template: '<a href=""><i tooltip="Back to home or playlist" tooltip-placement="bottom" class="icon-cancel-circle"></i></a>',
         link: function (scope, element, attrs) {
             element.on('click', function () {
                 scope.$apply(function () {
